59c1463194a338760d86e164c83b381fe0f0da35
flag3 <hts.a.y.225@gmail.com>
2023-02-04 19:51:31 +0900

    ToDoのコンポーネントを作る

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,15 @@
 import Counter from "./components/Counter";
+import ItemList from "./components/ItemList";
+import ToDo from "./components/ToDo";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <Counter />
+      <Counter />
+      <ItemList />
+      <ToDo />
     </div>
   );
 }
