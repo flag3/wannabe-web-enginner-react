a448631ba6254cd33b2f902314d3e66db3499fba
flag3 <hts.a.y.225@gmail.com>
2023-02-05 20:48:38 +0900

    ToDloリストデータを表示できるようにする

--- src/components/ToDoList.jsx
+++ src/components/ToDoList.jsx
@@ -1,16 +1,23 @@
 import ToDo from "./ToDo"
+import { useState } from "react";
+
 const ToDoList = () => {
+  const [todos] = useState([
+    { name: "あいうえお", stat: "未完了"},
+    { name: "かきくけこ", stat: "完了"},
+  ]);
   return (
     // ここにTodoコンポーネントを並べるl
     <div>
       <h2>ToDoリスト</h2>
       <ul>
-        <li>
-          <ToDo name="あいうえお" stat="未完了" />
-        </li>
-        <li>
-          <ToDo name="かきくけこ" stat="完了" />
-        </li>
+        {todos.map((todo) => {
+          return (
+            <li key={todo.name}>
+              <ToDo name={todo.name} stat={todo.stat} />
+            </li>
+          );
+        })}
       </ul>
     </div>
   );
